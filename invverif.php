7c7d315d84c7b0c0c0ca63bbdbb07a1ab2b8c077
Banu Ari Ramadhan <banuaril100@gmail.com>
2022-07-05 16:13:27 +0700

    05 07 2022

--- invverif.php
+++ invverif.php
@@ -417,71 +417,71 @@ function filterTable(sku){
 function cetakGeneric(mpi, rn, dn){
 		
 		// alert(mpi+'<br>'+rn+'<br>'+dn);		
-		// var number = 0;	
-		// var no = 1;	
+		var number = 0;	
+		var no = 1;	
 				
 		var html ='';	
 		html += 'No Document  : '+dn+' \n\r';
 		html += 'Rack         : '+rn+' \n\r \n\r';
 		// alert(html);
-		// $.ajax({
-			// url: "api/action.php?modul=inventory&act=cetak_generic",
-			// type: "POST",
-			// data : {mpi: mpi},
-			// success: function(dataResult){
-				// var html ='';	
-				// html += 'No Document  : \n\r';
-				// html += 'Rack         : \n\r \n\r';
-				// html += 'No | Nama / SKU | '+textbyline('Count',6,'right')+' | '+textbyline('Varian',6,'right')+' \n\r';
+		$.ajax({
+			url: "api/action.php?modul=inventory&act=cetak_generic",
+			type: "POST",
+			data : {mpi: mpi},
+			success: function(dataResult){
+
+				html += 'No Document  : \n\r';
+				html += 'Rack         : \n\r \n\r';
+				html += 'No | Nama / SKU | '+textbyline('Count',6,'right')+' | '+textbyline('Varian',6,'right')+' \n\r';
 			
 				
-				// var dataResult = JSON.parse(dataResult);
+				var dataResult = JSON.parse(dataResult);
 				
-				// var panjang = dataResult.length;
-				// $('#notif').html("Proses print");
+				var panjang = dataResult.length;
+				$('#notif').html("Proses print");
 				
-				// for(let i = 0; i < dataResult.length; i++) {
-						// let data = dataResult[i];
+				for(let i = 0; i < dataResult.length; i++) {
+						let data = dataResult[i];
 
-						// var sku = data.sku;
-						// var name = data.name;
-						// var qtyvariant = data.qtyvariant;
-						// var qtycount = data.qtycount;
+						var sku = data.sku;
+						var name = data.name;
+						var qtyvariant = data.qtyvariant;
+						var qtycount = data.qtycount;
 							
 							
-							// html += no+'. '+name+'\n\r';
-							// html +=textbyline(sku,1,'left')+' '+textbyline(''+qtycount+'',19-sku.length,'right')+' '+textbyline(''+qtyvariant+'',10,'right');
-							// html += "\n\r\n\r";
+							html += no+'. '+name+'\n\r';
+							html +=textbyline(sku,1,'left')+' '+textbyline(''+qtycount+'',19-sku.length,'right')+' '+textbyline(''+qtyvariant+'',10,'right');
+							html += "\n\r\n\r";
 
 								
 			
-							// number++;
-							// no++;
+							number++;
+							no++;
 							
 							
 							
-							// if(number == panjang){
+							if(number == panjang){
 							
-								// html+='\n\r';
-								// html+='\n\r';
-								// html+='\n\r';
-								// html+='\n\r';
-								// html+='\n\r';
-								// html+='\n\r';
+								html+='\n\r';
+								html+='\n\r';
+								html+='\n\r';
+								html+='\n\r';
+								html+='\n\r';
+								html+='\n\r';
 								print_text(html);
 								
-							// }
+							}
 					
 				
-				// }
+				}
 				
 				
 				
 				
 				
 				
-			// }
-		// });
+			}
+		});
 
 				
 }
