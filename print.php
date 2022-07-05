679df28454465ebf74914df1f06d812699017eba
Banu Ari Ramadhan <banuaril100@gmail.com>
2022-07-05 13:07:55 +0700

    05 07 2022

--- print.php
+++ print.php
@@ -3,18 +3,16 @@ $html = $_POST['html'];
 // $html = "TEST PRINNNTT";
 
 
-$myfile = fopen("print.txt", "w") or die("Unable to open file!");
-$txt = $html;
-fwrite($myfile, $txt);
-fclose($myfile);
-
-
-      $cmd='print.bat';
+// $myfile = fopen("print.txt", "w") or die("Unable to open file!");
+// $txt = $html;
+// fwrite($myfile, $txt);
+// fclose($myfile);
+// $cmd='print.bat';
 	  
 	  
 
-   // var cmd='';
-    // cmd='echo "'+html+'" | lpr -o raw';
+	$cmd='';
+    $cmd='echo "'.$html.'" | lpr -o raw';
 	
 	
 	
