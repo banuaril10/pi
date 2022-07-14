e4482f6ec607f60073118dacd5ab9851769ca96c
Banu Ari Ramadhan <banuaril100@gmail.com>
2022-07-14 11:22:29 +0700

    git cache cleared

--- cek_harga.php
+++ cek_harga.php
@@ -1,3 +1,8 @@
+<?php if($_SERVER["HTTPS"] != "on")
+{
+    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+    exit();
+} ?>
 <?php include "config/koneksi.php"; ?>
 <?php include "components/main.php"; ?>
 <?php include "components/sidebar.php"; ?>
