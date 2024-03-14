cc1cbe54346ade752c614e1abeca88894f1b9f20
Banu Ari Ramadhan <banuaril100@gmail.com>
2024-03-14 13:32:41 +0700

    12-03-2024

--- print_struk.php
+++ print_struk.php
@@ -1,48 +1,17 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
-use Mike42\Escpos\Printer;
-use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
-use Mike42\Escpos\PrintConnectors\FilePrintConnector;
-// $html = "PRINT PRINT
-// PRINT
-// PRINTPRINT
-// PRINT
-// PRINT
-// PRINT";
-
-
-
 $html = $_POST['html'];
-// $data = file_get_contents('http://localhost/pi/api/cek_printer.php');
-// echo $data;
-
-$ip_printer = $_POST['ip_printer'];
-
-
-
-
-try {
+	$cmd='';
+    $cmd='echo "'.$html.'" | lpr -o raw'; //linux
 	
 	
-	$connector = new FilePrintConnector("//".$ip_printer."/pos");
-
-    $printer = new Printer($connector);
-	$printer -> initialize();
-
-
-	$printer -> setFont(Printer::FONT_B);
-	$printer -> setTextSize(1, 1);
-	$printer -> text($html);
 	
-
-    $printer -> cut();
-    
-    $printer -> close();
+    $child = shell_exec($cmd); 
 	
 	
-	 echo "Proses Print\n";
-} catch (Exception $e) {
-    echo "Couldn't print to this printer: " . $e -> getMessage() . "\n";
-}
+	
+	$data = array("result"=>1, "msg"=>$child);
+		
+		$json_string = json_encode($data);	
+		echo $json_string;
 
 ?>
