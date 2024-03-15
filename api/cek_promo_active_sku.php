3a1e5acbe433dc51acb6b3cd68b1f2d040184575
Banu Ari Ramadhan <banuaril100@gmail.com>
2024-03-15 14:17:48 +0700

    push 2.2.6

--- api/cek_promo_active_sku.php
+++ api/cek_promo_active_sku.php
@@ -11,27 +11,34 @@ function rupiah($angka){
 $billno = $_GET['billno'];
 $sku = $_GET['sku'];
 // $billno = "BOSOL-140324S0001";
-$cek_qty = "select * from pos_dtempsalesline where billno = '".$billno."' and sku = '".$sku."' order by seqno desc limit 1";
-$cq = $connec->query($cek_qty);
+// $cek_qty = "select * from pos_dtempsalesline where billno = '".$billno."' and sku = '".$sku."' order by seqno desc limit 1";
+// $cq = $connec->query($cek_qty);
 $info = "Info Promo..";
 $nama_product = "";
 $discount_name = "";
-foreach($cq as $r){
+
+
+// foreach($cq as $r){
 	$notes = "";
-	$cek_grosir = "select discount, discountname from pos_mproductdiscountgrosir_new where sku = '".$r['sku']."' and DATE(now()) between fromdate and todate order by minbuy asc";
-	$cv = $connec->query($cek_grosir);
-	foreach ($cv as $r1){
-		$discount_name .= $r1['discountname'].', Potongan '.rupiah($r1['discount']).'/Pcs <br>';
-	}
 	
-	$product = "select * from pos_mproduct where sku = '".$r['sku']."'";
+	
+	$product = "select * from pos_mproduct where sku = '".$sku."'";
 	$cp = $connec->query($product);
 	foreach ($cp as $r1){
 		$nama_product = $r1['name'];
 	}
 	
-	$info = $nama_product.'<br> '.$discount_name.'';
-}
+	$cek_grosir = "select discount, discountname from pos_mproductdiscountgrosir_new where sku = '".$sku."' and DATE(now()) between fromdate and todate order by minbuy asc";
+	$cv = $connec->query($cek_grosir);
+	foreach ($cv as $r1){
+		$discount_name .= $r1['discountname'].', Potongan '.rupiah($r1['discount']).'/Pcs <br>';
+		$info = $nama_product.'<br> '.$discount_name.'';
+	}
+	
+	
+	
+	// $info = $nama_product.'<br> '.$discount_name.'';
+// }
 echo $info;
 
 
