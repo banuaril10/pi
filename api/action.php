38bb5f6f76282a4877fbbdfd07c864e4b38e2fb0
Banu Ari Ramadhan <banuaril100@gmail.com>
2024-05-08 07:41:10 +0700

    2024-05-06 fix topic & case study

--- api/action.php
+++ api/action.php
@@ -4017,7 +4017,7 @@ VALUES('".$item['ad_client_id']."', '".$item['ad_org_id']."', '1', '".date('Y-m-
 	}else if($_GET['act'] == 'cek_approval'){
 		
 	
-			$cek = $connec->query("select * from m_pi where date(insertdate) = '".date('Y-m-d')."' and status = '3'");
+			$cek = $connec->query("select * from m_pi where status = '3'");
 			$count = $cek->rowCount();
 				$no = 0;
 			
