576d9b79de5c55d98cb26217727f8b54de946c75
Banu Ari Ramadhan <banuaril100@gmail.com>
2025-01-01 11:50:35 +0700

    push

--- api/cyber/sync_users_it.php
+++ api/cyber/sync_users_it.php
@@ -1,43 +1,164 @@
 <?php
-include "../../config/koneksi.php";
+// include "../../config/koneksi.php";
 
-$ll = "select * from ad_morg where isactived = 'Y'";
-$query = $connec->query($ll);
+// $ll = "select * from ad_morg where isactived = 'Y'";
+// $query = $connec->query($ll);
 
-while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-    $idstore = $row['ad_morg_key'];
-}
-function get_category($url)
-{
-    $curl = curl_init();
-    curl_setopt_array(
-        $curl,
-        array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-        )
-    );
+// while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+//     $idstore = $row['ad_morg_key'];
+// }
+// function get_category($url)
+// {
+//     $curl = curl_init();
+//     curl_setopt_array(
+//         $curl,
+//         array(
+//             CURLOPT_URL => $url,
+//             CURLOPT_RETURNTRANSFER => true,
+//             CURLOPT_ENCODING => '',
+//             CURLOPT_MAXREDIRS => 10,
+//             CURLOPT_TIMEOUT => 0,
+//             CURLOPT_FOLLOWLOCATION => true,
+//             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//             CURLOPT_CUSTOMREQUEST => 'GET',
+//         )
+//     );
 
-    $response = curl_exec($curl);
-    curl_close($curl);
-    return $response;
-}
-$url = $base_url . '/store/users/get_users_it.php';
+//     $response = curl_exec($curl);
+//     curl_close($curl);
+//     return $response;
+// }
+// $url = $base_url.'/store/users/get_users_it.php';
 
-// echo $idstore;
+// // echo $idstore;
 
-$hasil = get_category($url);
-$j_hasil = json_decode($hasil, true);
+// $hasil = get_category($url);
+// $j_hasil = json_decode($hasil, true);
+
+
+
+$myarray = array(
+    array(
+        'id_user' =>
+            'C3DA7B7F76AC430897531621291542A4',
+        'id_location' => null,
+        'insertdate' => '2024-10-28 07:15:42',
+        'insertby' => 'iv',
+        'postby' => 'iv',
+        'postdate' => '2024-10-28 07:15:42',
+        'id_role' => '2f9f5a6a-d183-4887-a1a0-8efca0800e9e',
+        'fullname' => 'Testing IN - 01',
+        'username' => 'test-user-01',
+        'avatar' => 'UserNoFrame.png',
+        'password' => '67e6d3f13fdd0ad6dd97dd0fde16362f66fc114fbc141672ffb01cc92d7b739a',
+        'email' => null,
+        'phone' => '10100110',
+        'description' => null,
+        'is_active' => '1',
+        'accesscode' => '35',
+        'unicode' => '25',
+        'auth
+        _key' => 'da8647963ebca88e3254632cd2ed1a86',
+
+        'access_token' => 'd6ad6a2a9d0909c9a7303b3dabf32f08'
+    ),
+    array(
+        'id_user' => 'E3BF8D790EE24E7FBA8300D239791380',
+        'id_location' => null,
+        'insertdate' => '2024-10-30 13:14:05.306588',
+        'insertby' => null,
+        'postby' => null,
+        'postdate' => null,
+        'id_role' => '2f9f5a6a-d183-4887-a1a0-8efca0800e9e',
+        'fullname' => 'Tommy Zulkhaidir',
+        'username' => '11166',
+        'avatar' => 'UserNoFrame.png',
+        'password' => 'a298fcf74a4db1bbf82f8f9971d09f319e11066c612e7df4d0768f9b8d3083ff',
+        'email' => null,
+        'phone' => '081319010432',
+        'description' => null,
+        'is_active' => '1',
+        'accesscode' => '1',
+        'unicode' => '1',
+        'auth_key' => null,
+        'access_token' => null
+    ),
+    array(
+        'id_user' => '7ad5141cd9ac424483a1687d4d4b8663',
+        'id_location' => null,
+        'insertdate' => '2024-08-12 09:34:06.946',
+        'insertby' => 'IRFAN',
+        'postby' => 'IRFAN',
+        'postdate' => '2024-08-12 09:34:06.946',
+        'id_role' => '2f9f5a6a-d183-4887-a1a0-8efca0800e9e',
+        'fullname' => 'Banu',
+        'username' => 'banu',
+        'avatar' => 'UserNoFrame.png',
+        'password' => '0c151eda7f88d60345c1eaf4cb622532abad2bda6b24876999a8c092fa5092c8',
+        'email' => '',
+        'phone' => '088211832975',
+        'description' => '',
+        'is_active' => '1',
+        'accesscode' => null,
+        'unicode' => null,
+        'auth_key' => 'dbM89Sv1VftfXUeAPxeK1wZKyRU1ZuT9',
+        'access_token' => 'JfJUp7MLb6uCUd3ZvafM9OVq5bkkIikW'
+    ),
+    array(
+        'id_user' => 'E05BD
+        54B73A74BC5919B2A7541063627',
+        'id_location' => null,
+        'insertdate' => '2024-12-31 15:27:02',
+        'insertby' => 'iv',
+        'postby' => 'iv',
+        'postdate' => '2024-12-31 15:27:02',
+        'id_role' => '2f9f5a6a-d183-4887-a1a0-8efca0800e9e',
+        'fullname' => 'Yusup Alfarizi
+        ',
+        'username' => '171751',
+        'avatar' => 'UserNoFrame.png',
+        'password' => '3b42ad7d28df89e97b8c9ef4c3a3cb53ba8fb5cb0ac93b11c2fd3045b66484a9',
+        'email' => null,
+        'phone' => '087720929268',
+        'description' => null,
+        'is_active' => '1',
+        'accesscode' => null,
+        'unicode' => null,
+        'auth
+        _key' => 'da8647963ebca88e3254632cd2ed1a86',
+        'access_token' => 'd6ad6a2a9d0909c9a7303b3dabf32f08'
+    ),
+    array(
+        'id_user' => 'b07e559075a241df8973ea5bfc9574fc',
+        'id_location' => null,
+        'insertdate' => '2024-08-12 09:34:06.946',
+        'insertby' => 'IRFAN',
+        'postby' => 'IRFAN',
+        'postdate' => '2024-08-12 09:34:06.946',
+        'id_role' => '2f9f5a6a-d183-4887-a1a0-8efca0800e9e',
+        'fullname' => 'Nada F',
+        'username' => 'nada',
+        'avatar' => 'UserNoFrame.png',
+        'password' => 'ad97c8aafa71436942e5d0462f12bc8af44b53806e7dac69e8a597e2efed65f4',
+        'email' => '',
+        'phone' => '081316830855',
+        'description' => '',
+        'is_active' => '1',
+        'accesscode' => null,
+        'unicode' => null,
+        'auth_key' => '88Dnv4I4RFcabH6SLWsJjcRUKes58dv1',
+        'access_token' => 'KG76lVARX5asrVp0dybjTRyQthnFJqRH'
+    )
+);
+
+$jsonData = json_encode($myarray);
+// echo $jsonData;
+
+
 
 $json = array(
     "status" => "OK",
-    "data" => $j_hasil
+    "data" => $jsonData
 );
 
 echo json_encode($json);
