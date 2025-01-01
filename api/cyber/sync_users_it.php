68ef6a7d733ac0e760324c5b532bc351632f8d92
Banu Ari Ramadhan <banuaril100@gmail.com>
2025-01-01 10:18:41 +0700

    push

--- api/cyber/sync_users_it.php
+++ api/cyber/sync_users_it.php
@@ -52,57 +52,71 @@ if ($jum == 0) {
     echo json_encode($json);
 }else{
 
-    $id_user = "pos";
-    $id_location = $idstore;
-    $insertdate = date('Y-m-d H:i:s');
-    $insertby = "pos";
-    $postby = "pos";
-    $postdate = date('Y-m-d H:i:s');
-    $id_role = "2f9f5a6a-d183-4887-a1a0-8efca0800e9e";
-    $fullname = "POS";
-    $username = "pos";
-    $avatar = "pos";
-    $password = "pos";
-    $email = "pos";
-    $phone = "pos";
-    $description = "pos";
-    $is_active = "Y";
-    $accesscode = "pos";
-    $unicode = "pos";
-    $auth_key = "pos";
-    $access_token = "pos";
+    // $id_user = "pos";
+    // $id_location = $idstore;
+    // $insertdate = date('Y-m-d H:i:s');
+    // $insertby = "pos";
+    // $postby = "pos";
+    // $postdate = date('Y-m-d H:i:s');
+    // $id_role = "2f9f5a6a-d183-4887-a1a0-8efca0800e9e";
+    // $fullname = "POS";
+    // $username = "pos";
+    // $avatar = "pos";
+    // $password = "pos";
+    // $email = "pos";
+    // $phone = "pos";
+    // $description = "pos";
+    // $is_active = "Y";
+    // $accesscode = "pos";
+    // $unicode = "pos";
+    // $auth_key = "pos";
+    // $access_token = "pos";
 
 
-    $myarray = array();
-    $myarray[] = array(
-        'id_user' => $id_user,
-        'id_location' => $idstore,
-        'insertdate' => $insertdate,
-        'insertby' => $insertby,
-        'postby' => $postby,
-        'postdate' => $postdate,
-        'id_role' => $id_role,
-        'fullname' => $fullname,
-        'username' => $username,
-        'avatar' => $avatar,
-        'password' => $password,
-        'email' => $email,
-        'phone' => $phone,
-        'description' => $description,
-        'is_active' => $is_active,
-        'accesscode' => $accesscode,
-        'unicode' => $unicode,
-        'auth_key' => $auth_key,
-        'access_token' => $access_token
-    );
+    // $myarray = array();
+    // $myarray[] = array(
+        // 'id_user' => $id_user,
+        // 'id_location' => $idstore,
+        // 'insertdate' => $insertdate,
+        // 'insertby' => $insertby,
+        // 'postby' => $postby,
+        // 'postdate' => $postdate,
+        // 'id_role' => $id_role,
+        // 'fullname' => $fullname,
+        // 'username' => $username,
+        // 'avatar' => $avatar,
+        // 'password' => $password,
+        // 'email' => $email,
+        // 'phone' => $phone,
+        // 'description' => $description,
+        // 'is_active' => $is_active,
+        // 'accesscode' => $accesscode,
+        // 'unicode' => $unicode,
+        // 'auth_key' => $auth_key,
+        // 'access_token' => $access_token
+    // );
 
-    $jsonData = json_encode($myarray);
-    $j_hasil = json_decode($jsonData, true);
+    // $jsonData = json_encode($myarray);
+    // $j_hasil = json_decode($jsonData, true);
+
+    // $json = array(
+        // "status" => "OK",
+        // "message" => $j_hasil,
+    // );
+    // echo json_encode($json);
+	
+	 $url = $base_url . '/store/users/get_users_it.php';
+
+    // echo $idstore;
+
+    $hasil = get_category($url);
+    $j_hasil = json_decode($hasil, true);
 
     $json = array(
         "status" => "OK",
-        "message" => $j_hasil,
+        "data" => $j_hasil
     );
+
     echo json_encode($json);
 }
 
