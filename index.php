c125d07b2d1ad2d8855e1761122516703cc1e15d
Banu Ari Ramadhan <banuaril100@gmail.com>
2025-05-27 14:55:54 +0700

    login dll

--- index.php
+++ index.php
@@ -99,7 +99,7 @@
 			if(dataResults.result=='1'){
 				$('#notif1').html("<font style='color: green'>Version up to date (ver "+dataResults.version+") "+
 				"<a target=_blank href='https://pi.idolmartidolaku.com/api/live/pi/doc_pi.php'>Link update</a> &nbsp "+
-				"<button type='button' onclick='updateVersion();' class='btn btn-danger'>Update</button></font> ");
+				"</font> ");
 				$(':input[type="submit"]').prop('disabled', false);
 			}else{
 				
@@ -112,8 +112,7 @@
 					
 				}
 				
-				$('#notif1').html(msg+" <a target=_blank href='https://pi.idolmartidolaku.com/api/live/pi/doc_pi.php'>Link update</a> &nbsp <button type='button'"+
-					"onclick='updateVersion();' class='btn btn-danger'>Update</button> ");
+				$('#notif1').html(msg+" <a target=_blank href='https://pi.idolmartidolaku.com/api/live/pi/doc_pi.php'>Link update</a> ");
 				// $(':input[type="submit"]').prop('disabled', true);
 				$(':input[type="submit"]').prop('disabled', false);
 			}
