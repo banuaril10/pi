7a7da0fe4e33d835113470416e33290d1cda5294
Banu Ari Ramadhan <banuaril100@gmail.com>
2025-08-07 14:23:02 +0700

    2025-08-04

---
+++ api/cyber/check_lpk.php
@@ -0,0 +1,21 @@
+<?php
+include "../../config/koneksi.php";
+
+$tanggalHariIni = date('Y-m-d');
+
+// Query cek status
+$sql = "
+    SELECT 1
+    FROM pos_dshopsales
+    WHERE status = 'DONE'
+      AND salesdate::date = :today
+    LIMIT 1
+";
+
+$stmt = $connec->prepare($sql);
+$stmt->execute(['today' => $tanggalHariIni]);
+$data = $stmt->fetch();
+
+echo $data ? "1" : "0";
+
+?>
