2cdbd178689874e14ee970307d01a956a7be610e
Banu Ari Ramadhan <banuaril100@gmail.com>
2025-10-03 14:19:17 +0700

    2025-10-02 open email LIA

--- create_table.php
+++ create_table.php
@@ -901,6 +901,13 @@ foreach ($create_table_combo as $r) {
 }
 
 
+$alter_tempsales = [
+	'ALTER TABLE public.pos_dtempsalesline ADD COLUMN IF NOT EXISTS reference_id varchar NULL;'
+];
+foreach ($alter_tempsales as $r) {
+	$connec->exec($r);
+}
+
 
 ?>
 
